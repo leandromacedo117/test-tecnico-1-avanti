aacfaeb74d61f9da82e3096053ed9340cc6492ec
leeandromacedo117 <leeandromacedo117@gmail.com>
2025-04-04 12:22:23 -0300

    Refatoração do Estilo

--- scripts/productSlide.js
+++ scripts/productSlide.js
@@ -1,7 +1,7 @@
-var swiper = new Swiper(".mySwiper", {
+var shelf = new Swiper(".product-shelf", {
     spaceBetween: 17,  
     centeredSlides: false,
-    slidesPerView: 6,
+    slidesPerView: 5,
     loop: true,
     autoplay: {
       delay: 2500,
@@ -14,9 +14,24 @@ var swiper = new Swiper(".mySwiper", {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
-    },
+    }, breakpoints: {
+      390:{
+        slidesPerView: 2
+      }, 1000: {
+        slidesPerView:5
+      }
+    }
     
   }); 
+
+
+  var swiper = new Swiper(".tipbar", {
+    spaceBetween: 17,  
+    centeredSlides: false,
+    slidesPerView: 1,
+    loop: true,
+   
+});
 
   
 
