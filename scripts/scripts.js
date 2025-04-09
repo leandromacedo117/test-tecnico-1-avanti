1cd5e1dcdb1589e4fcc6262720ec99c749e4b5ef
leeandromacedo117 <leeandromacedo117@gmail.com>
2025-04-09 09:41:11 -0300

    Menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,5 @@
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+const menuMobile = document.getElementById('menu-mobile__icon')
+
+menuMobile.addEventListener("click", (e)=>{
+  
+})
